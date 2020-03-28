181f1a323fb0cb9b677f0c949c05f557aafd30bc
Kyle Treptow <treptow@apollointeractive.com>
2020-03-28 10:19:54 -0700

    sandbox session 1

--- components/piano.vue
+++ components/piano.vue
@@ -1,5 +1,12 @@
 <template>
-  <p class="piano">Piano!</p>
+  <div>
+    <p class="piano">{{ title }}</p>
+    <div class="">
+      <button v-on:click="playnote('C', '8n')">C</button>
+      <button v-on:click="playnote('D', '8n')">D</button>
+      <button v-on:click="playnote('E', '8n')">E</button>
+    </div>
+  </div>
 </template>
 
 <style lang="sass">
@@ -8,3 +15,23 @@
     color: #666
     padding: $blh
 </style>
+
+<script>
+  import Tone from 'tone'
+  if (process.client) {
+    var synth = new Tone.Synth().toMaster()
+  }
+  export default {
+    name: 'piano',
+    data () {
+      return {
+        title: "Kyle's Piano (WIP)"
+      }
+    },
+    methods: {
+      playnote: function (note, time) {
+        synth.triggerAttackRelease(note, time)
+      }
+    }
+  }
+</script>
