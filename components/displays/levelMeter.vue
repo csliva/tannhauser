99b6062d9079d3dca54292076d1837fcface0b63
csliva <colt.sliva@gmail.com>
2020-06-07 12:49:39 -0400

    meter

--- components/displays/levelMeter.vue
+++ components/displays/levelMeter.vue
@@ -1,8 +1,8 @@
 <template lang="html">
 <div>
-  <div class="meter" :style="{ transform: 'scaleX('+ lerp +')'}">
+  <div class="metr-wrap">
+    <div class="metr" :style="{ width: + lerp + '%' }"></div>
   </div>
-  <p>val: {{ lerp }}</p>
 </div>
 </template>
 
@@ -11,21 +11,21 @@
     props: [ 'value' ],
     computed: {
       lerp: function () {
-        return ((this.value*100)/24)*-1
+        return ( ((parseFloat(this.value) + 24)/48)*100 )
       }
     }
   }
 </script>
 
 <style lang="sass">
-  .meter
+  .metr-wrap
       display: block
       height: 10px
-      &:after
-        content: ""
-        background-color: red
-        height: 100%
-        width: 1%
-        position: absolute
-        right: 50%
+      width: 100%
+      background-color: #1b203d
+      box-shadow: inset 0 0 4px #13172b
+  .metr    
+    background: linear-gradient(to right, #49ac8b, #41c0fa)
+    display: block
+    height: 100%
 </style>
